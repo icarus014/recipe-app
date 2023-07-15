d2423f0868a04f1dadb36e1b669303d6280f7aa6
Darian Brown <browndarian011@gmail.com>
2023-07-15 11:58:38 -0400

    created login form

--- client/src/pages/auth.js
+++ client/src/pages/auth.js
@@ -1,6 +1,40 @@
+import { useState } from 'react'
+
 export const Auth = () =>{
     return(
-        <div>Hello Auth</div>
+        <div>
+            <Login />
+            <Register />
+        </div>
     )
 
+}
+
+const Login = ( )=>{
+    return(
+        <div>
+            Login
+        </div>
+    )
+}
+const Register = ( )=>{
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+
+    return(
+        <div>
+            <form>
+                <h2>Register</h2>
+                <div>
+                    <label htmlFor="username">Username</label>
+                    <input type="text" id="username" value={username} onChange={(event) =>setUsername(event.target.value)}/>
+                </div>
+                <div>
+                    <label htmlFor="password">Password</label>
+                    <input type="text" id="password" value={password} onChange={(event) => setPassword(event.target.value)}/>
+                </div>
+                <button type="submit">Register</button>
+            </form>
+        </div>
+    )
 }
