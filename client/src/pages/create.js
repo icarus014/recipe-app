c4f87e1117e87fdceaee9d47eb81bfb6b9a03cff
Darian Brown <browndarian011@gmail.com>
2023-07-17 21:23:54 -0400

    started recipe form

--- client/src/pages/create.js
+++ client/src/pages/create.js
@@ -1,6 +1,31 @@
-export const Create = () =>{
+export const Create = () => {
     return(
-        <div>Hello create</div>
+        <div classname='create-recipe'>
+            <h2>
+                Create Your Recipe
+            </h2>
+            <form>
+                <label htmlFor="name">Name</label>
+                <input type="text" id="name"/>
+                <br/>
+                <label htmlFor="description">Ingredients</label>
+                <br/>
+                <label htmlFor="description">Description</label>
+                <textarea id="description" name="description"></textarea>
+                <br/>
+                <br/>
+                <label htmlFor="instructions">Instructions</label>
+                <textarea id="instructions" name="instructions"></textarea>
+                <br/>
+                <br/>
+                <label htmlFor="imageUrl">Image URL</label>
+                <input type="text" id="imageUrl"name="imageUrl"/>
+                <br/>
+                <br/>
+                <label htmlFor="cookingTime">Cook Time(minutes)</label>
+                <input type="number" id="cookingTime" name="cookingTime"/>
+            </form>
+        </div>
     )
 
 }
