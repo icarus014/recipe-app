52f05e35a84e1ca7a052b9deec36f30b019abfa7
Darian Brown <browndarian011@gmail.com>
2023-07-19 12:21:13 -0400

    added ingredients button

--- client/src/pages/create.js
+++ client/src/pages/create.js
@@ -15,31 +15,65 @@ export const Create = () => {
         const{name,value} = event.target;
         setRecipe({...recipe, [name]:value})
     }
+    // handle change specific for ingredient
+    const handleIngredientChange = (event, idx)=>{
+        const{value} = event.target;
+        const ingredients = recipe.ingredients
+        // changing to value of event 
+        ingredients[idx] = value;
+        setRecipe({...recipe, ingredients})
+       
+    }
+    
+    
+    // getting all properties from ingredients array
+    const addIngredient = () => {
+        setRecipe({...recipe, ingredients: [...recipe.ingredients, ""]})
+    }
+    console.log(recipe)
     return(
         <div classname='create-recipe'>
             <h2>
                 Create Your Recipe
             </h2>
             <form>
+                {/* Name */}
                 <label htmlFor="name">Name</label>
                 <input type="text" id="name" name="name" onChange={handleChange}/>
                 <br/>
+                {/* ingredients */}
+                <br/>
                 <label htmlFor="description">Ingredients</label>
                 <br/>
+                {recipe.ingredients.map((ingredient, idx)=>(
+                    <input key={idx} type="text" name="ingredients" value={ingredient} onChange={(event)=>handleIngredientChange(event,idx)}/>
+                ))}
+                <br/>
+                <button onClick={addIngredient} type="button">Add Ingredients</button>
+                <br/>
+                <br/>
+                {/* Description */}
                 <label htmlFor="description">Description</label>
                 <textarea id="description" name="description" onChange={handleChange}></textarea>
                 <br/>
                 <br/>
+                {/* Instructions */}
                 <label htmlFor="instructions">Instructions</label>
                 <textarea id="instructions" name="instructions" onChange={handleChange}></textarea>
                 <br/>
                 <br/>
+                {/* image URL */}
                 <label htmlFor="imageUrl">Image URL</label>
                 <input type="text" id="imageUrl"name="imageUrl" onChange={handleChange}/>
                 <br/>
                 <br/>
+                {/* Cooking Time */}
                 <label htmlFor="cookingTime">Cook Time(minutes)</label>
                 <input type="number" id="cookingTime" name="cookingTime" onChange={handleChange}/>
+                    <br/>
+                    <br/>
+                    <br/>
+                <button type="submit">Create Recipe</button>
             </form>
         </div>
     )
