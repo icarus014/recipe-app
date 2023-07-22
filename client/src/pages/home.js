1b75349457299ed235d7fb22fec59552fe333cdf
Darian Brown <browndarian011@gmail.com>
2023-07-21 21:30:17 -0400

    finished homepage

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -49,31 +49,49 @@ export const Home = () => {
 
   return (
     <div className="flex flex-col">
-        <h1 className="font-bold text-white mx-auto mb-5 text-4xl">Recipes</h1>
+      <h1 className="font-bold text-white mx-auto mb-5 text-4xl">Recipes</h1>
       <div className="max-w-md rounded overflow-hidden shadow-xl">
         {recipes.map((recipe) => (
           <ul key={recipe._id}>
-            <div className="bg-white px-14 pb-7 rounded-xl ">
-              <h2 className="font-bold text-stone-500 mx-auto mb-5 text-2xl">{recipe.name}</h2>
-              <button 
-              onClick={() => saveRecipe(recipe._id)}
-              disabled={isRecipeSaved(recipe._id)}
-              className="bg-slate-500 hover:bg-blue-400 rounded-md mx-2 mb-10 font-bold py-1 px-2 text-lg focus:outline-none focus:shadow-outline">
-              
-                {isRecipeSaved(recipe._id)? "Saved": "Save"}
-              </button>
-            <img src={recipe.imageUrl} alt={recipe.name}  className="rounded-xl max-w-xs mx-auto"/>
+            <div className="bg-white px-14 pb-7 rounded-xl">
+              {/* RECIPE NAME */}
+              <div className="flex">
+                <h2 className="font-bold text-stone-500 mx-auto mb-5 text-2xl">{recipe.name}</h2>
+              </div>
+              <div className="flex ">
+                <button 
+                onClick={() => saveRecipe(recipe._id)}
+                disabled={isRecipeSaved(recipe._id)}
+                className="bg-slate-500 hover:bg-blue-400 rounded-md mx-auto mb-10 font-bold text-lg px-2 focus:outline-none focus:shadow-outline shadow-xl border">
+                  {isRecipeSaved(recipe._id)? "Saved": "Save"}
+                </button>
+              </div>
+              {/* IMAGE */}
+              <div className="flex-col flex">
+                <img src={recipe.imageUrl} alt={recipe.name}  className="rounded-xl max-w-sm mb-4 border "/>
+              </div>
             </div>
-            <div className="text-gray-400">
-              <ul>
-                <li className="font-bold">
-                Ingredients: 
-                 <p className="text-md">
-                  {recipe.ingredients}
-                  </p>
+            <div className="text-gray-400 font-bold w-">
+              <ul className="bg-gray-700 rounded-xl mt-5 px-5">
+                {/* DESCRIPTION */}
+                <li className="mb-3">
+                  Description: 
+                  <p>{recipe.description}</p>
                 </li>
-                <li>{recipe.instructions}</li>
-                <li className="mb-6">Cook Time: {recipe.cookingTime} minutes</li>
+                {/* INGREDIENTS */}
+                <li className="font-bold text-md mb-4">
+                Ingredients: 
+                <p>
+                  {recipe.ingredients}
+                </p>
+                </li>
+                {/* COOK TIME */}
+                <li className="mb-6 font-bold text-md">Cook Time: {recipe.cookingTime} minutes</li>
+                {/* INSTRUCTIONS */}
+                <li className="mb-4 font-bold text-md">
+                  Instructions:
+                  <p> {recipe.instructions}</p>
+                </li>
               </ul>
             </div>
           </ul>
